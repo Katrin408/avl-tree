4553cc8b576b3a01c96a1ebc3edc17840ca45244
Alexander Tischenko <sashaaero@yandex.ru>
2017-12-24 01:58:49 +0300

    pep8 avl.py new to_list method get rid of set/get methods

--- src/avl.py
+++ src/avl.py
@@ -8,155 +8,92 @@ Created on Fri Nov 10 02:26:42 2017
 import random
 
 
-class AVLTreeNode:
-    u"Tree node"
+class Node:
 
     def __init__(self, value):
-        self.setValue(value)
-        self.setHeight(1)
-        self.setLeft(None)
-        self.setRight(None)
+        self.value = value
+        self.height = 1
+        self.left = None
+        self.right = None
 
     def __str__(self):
         return str(self.value)
 
-    def getLeft(self):
-        return self.left
+    def fix_height(self):
+        left_height = self.left.height if self.left else 0
+        right_height = self.right.height if self.right else 0
+        self.height = 1 + max(left_height, right_height)
 
-    def setLeft(self, left):
-        self.left = left
-
-    def getRight(self):
-        return self.right
-
-    def setRight(self, right):
-        self.right = right
-
-    def getValue(self):
-        return self.value
-
-    def setValue(self, value):
-        self.value = value
-
-    def getHeight(self):
-        return self.height
-
-    def setHeight(self, height):
-        self.height = height
-
-    def fixHeight(self):
-        leftHeight = self.left.getHeight() if self.left else 0
-        rightHeight = self.right.getHeight() if self.right else 0
-        self.setHeight(1 + max(leftHeight, rightHeight))
-
-    def getBalanceFactor(self):
-        return (self.right.getHeight() if self.right else 0) - \
-               (self.left.getHeight() if self.left else 0)
+    def get_balance_factor(self):
+        return (self.right.height if self.right else 0) - \
+               (self.left.height if self.left else 0)
 
 
 class AVLTree:
     def __init__(self):
         self.node = None
-        self.count = 0
 
     def add(self, value):
-        self.node = self.__insertNode(self.node, value)
+        self.node = self.insert(self.node, value)
 
-    def toList(self):
-        return self.__toList(self.node)
+    def to_list(self):
+        node = self.node
+        tree = []
 
-    def toTreeList(self):
-        return self.__toTreeList(self.node)
+        def extract(node):
+            if node:
+                tree.append(node)
+                if node.left:
+                    extract(node.left)
+                if node.right:
+                    extract(node.right)
+        extract(node)
 
-    def __toList(self, node):
-        if not node:
-            return None
-        result = []
-        if node.getLeft():
-            result += self.__toList(node.getLeft())
-        result += [node.getValue()]
-        if node.getRight():
-            result += self.__toList(node.getRight())
-        return result
+        return tree
 
-    def __toTreeList(self, node):
-        if not node:
-            return None
-        result = []
-        if node.getLeft():
-            result.append(self.__toTreeList(node.getLeft()))
-        result.append(node.getValue())
-        if node.getRight():
-            result.append(self.__toTreeList(node.getRight()))
-        return result
-
-    def __insertNode(self, root, value):
+    def insert(self, root, value):
         if not root:
-            return AVLTreeNode(value)
-        if (value < root.getValue()):
-            root.setLeft(self.__insertNode(root.getLeft(), value))
+            return Node(value)
+        if value < root.value:
+            root.left = self.insert(root.left, value)
         else:
-            root.setRight(self.__insertNode(root.getRight(), value))
-        return self.__balance(root)
+            root.right = self.insert(root.right, value)
+        return self.balance(root)
 
-    def __rotateRight(self, node):
-        pivot = node.getLeft()
-        node.setLeft(pivot.getRight())
-        pivot.setRight(node)
-        node.fixHeight()
-        pivot.fixHeight()
+    @staticmethod
+    def rotate_right(node):
+        pivot = node.left
+        node.left = pivot.right
+        pivot.right = node
+        node.fix_height()
+        pivot.fix_height()
         return pivot
 
-    def __rotateLeft(self, node):
-        pivot = node.getRight()
-        node.setRight(pivot.getLeft())
-        pivot.setLeft(node)
-        node.fixHeight()
-        pivot.fixHeight()
+    @staticmethod
+    def rotate_left(node):
+        pivot = node.right
+        node.right = pivot.left
+        pivot.left = node
+        node.fix_height()
+        pivot.fix_height()
         return pivot
 
-    def __balance(self, node):
-        node.fixHeight()
-        nodeBalanceFactor = node.getBalanceFactor()
-        if nodeBalanceFactor == 2:
-            if node.getRight() and (node.getRight().getBalanceFactor() < 0):
-                node.setRight(self.__rotateRight(node.getRight()))
-            return self.__rotateLeft(node)
-        if nodeBalanceFactor == -2:
-            if node.getLeft() and (node.getLeft().getBalanceFactor() > 0):
-                node.setLeft(self.__rotateLeft(node.getLeft()))
-            return self.__rotateRight(node)
+    def balance(self, node):
+        node.fix_height()
+        node_balance_factor = node.get_balance_factor()
+        if node_balance_factor == 2:
+            if node.right and (node.right.get_balance_factor() < 0):
+                node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
+        if node_balance_factor == -2:
+            if node.left and (node.left.get_balance_factor() > 0):
+                node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
         return node
-
-def traverse(root):
-    current_level = [root]
-    while current_level:
-        print(' '.join(str(node) for node in current_level))
-        next_level = []
-        for n in current_level:
-            if n.left:
-                next_level.append(n.left)
-            if n.right:
-                next_level.append(n.right)
-            current_level = next_level
-
-
-def tree_to_list(node):
-    tree = []
-
-    def extract(node):
-        if node:
-            tree.append(node)
-            if node.left:
-                extract(node.left)
-            if node.right:
-                extract(node.right)
-    extract(node)
-
-    return tree
 
 
 if __name__ == "__main__":
+    from drawtree import draw_bst
     tree = AVLTree()
     values = list(range(16))
     random.shuffle(values)
@@ -164,6 +101,5 @@ if __name__ == "__main__":
     for i in range(16):
         tree.add(values[i])
 
-    tree = [node.value for node in tree_to_list(tree.node)]
-    from drawtree import draw_bst
-    draw_bst(tree)
+    tree_ = [node.value for node in tree.to_list()]
+    draw_bst(tree_)
