d4c0980a3315073e53923c1007e1359b373ce1c2
Alexander Tischenko <sashaaero@yandex.ru>
2017-12-25 01:52:09 +0300

    New method of drawing

--- src/tutorial.py
+++ src/tutorial.py
@@ -10,6 +10,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import matplotlib
 from matplotlib.figure import Figure
+from matplotlib.patches import Ellipse
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
@@ -24,62 +25,53 @@ class Window(QMainWindow, form_class):
     def __init__(self):
         super(Window, self).__init__()
         self.setupUi(self)
-        self.home()
 
-    def home(self):
-        self.axes = self.mpl_figure.add_axes([0.1, 0.1, 0.8, 0.8])
-        # self.axes = self.mpl_figure.add_subplot(111)
+        self.axes = self.mpl_figure.add_subplot(111)
 
         self.canvas = FigureCanvas(self.mpl_figure)
         self.mplVL.addWidget(self.canvas)
         self.canvas.draw()
-
-        # self.toolbar = NavigationToolbar(self.canvas, self.MPLWindow, coordinates = True)
-        # self.mplVL.addWidget(self.toolbar)
-
         self.toolbar = NavigationToolbar(self.canvas, self, coordinates=True)
         self.addToolBar(self.toolbar)
 
         self.AddButton.clicked.connect(self.AddButton_OnClick)
         self.LineList.itemClicked.connect(self.LineList_OnClick)
 
+
+        self.axes.annotate("1",
+                  xy=(0.2, 0.2), xycoords='data',
+                  xytext=(0.8, 0.8), textcoords='data',
+                  size=20, va="center", ha="center",
+                  bbox=dict(boxstyle="circle", fc="w"),
+                  arrowprops=dict(arrowstyle="->",
+                                  connectionstyle="arc3,rad=-0.2",
+                                  fc="w"),)
+
+
+
         self.show()
 
     def AddButton_OnClick(self):
-        x1_c = self.x1_lineEdit.text()
-        y1_c = self.y1_lineEdit.text()
-        x2_c = self.x2_lineEdit.text()
-        y2_c = self.y2_lineEdit.text()
 
-        key = '([' + x1_c + ';' + y1_c + '],[' + x2_c + ';' + y2_c + '])'
-        x_coord = [int(x1_c), int(x2_c)]
-        y_coord = [int(y1_c), int(y2_c)]
+        self.axes.annotate("2",
+                           xy=(0.2, 0.2),
+                           xytext=(0.2, 0.2), textcoords='data',
+                           size=20, va="center", ha="center",
+                           bbox=dict(boxstyle="circle", fc="w"),
+                           )
 
-        self.lines[key] = [x_coord, y_coord]
 
-        temp_list = self.axes.plot(x_coord, y_coord, color='blue');
-
-        for line in temp_list:
-            self.lines_pic[key] = line
-
-        self.LineList.addItem(key)
         self.canvas.draw()
 
     def LineList_OnClick(self, item):
-        key = item.text()
-        for i, line in self.lines_pic.items():
-            line.set_color('blue')
-        self.lines_pic[key].set_color('red')
-
         self.canvas.draw()
 
 
 def run():
     app = QApplication(sys.argv)
 
-    GUI = Window()
-    # sys.exit(app.exec_())
-    app.exec_()
+    Window()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
